89e9426a5347005b5f9164de09a6e518f95b47f8
Kyoko Komiyama <122721050+KyokoKomiyama@users.noreply.github.com>
2023-04-17 19:51:58 +0900

    Update BPockets & Wallet.py

--- pages/BPockets & Wallet.py
+++ pages/BPockets & Wallet.py
@@ -82,7 +82,7 @@ def scrape_product_info(url):
         chrome_service = fs.Service(executable_path=ChromeDriverManager().install())
 
         # option設定を追加（設定する理由はメモリの削減）
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
